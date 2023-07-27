2699d13ea36e81d957294ba9b008a2dcb9064b3c
wza <1271943726@qq.com>
2023-07-27 14:00:02 +0800

    second commit

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo2Application {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         SpringApplication.run(Demo2Application.class, args);
     }
 
