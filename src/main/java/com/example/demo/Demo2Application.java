d557fb36ab397a11be4d89809bbd1b8c670106b3
wza <1271943726@qq.com>
2023-07-28 06:38:14 +0800

    commit

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -9,6 +9,7 @@ public class Demo2Application {
     public static void main(String[] args) {
         System.out.println("hotfix");
         System.out.println("hotfix2");
+        System.out.println("hotfix3");
         SpringApplication.run(Demo2Application.class, args);
     }
 
