17bbca99f04d35d4362a5b32fa331713b03ba286
wza <1271943726@qq.com>
2023-07-28 06:39:00 +0800

    commit

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo2Application {
 
     public static void main(String[] args) {
-        System.out.println("hotfix");
-        System.out.println("hotfix2");
-        System.out.println("hotfix3");
         SpringApplication.run(Demo2Application.class, args);
     }
 
